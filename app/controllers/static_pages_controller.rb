e469cc215a385130d1f41ad5aa8c01a15071a8a9
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-25 15:35:35 -0400

    Finish by adding search for collection_id

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,13 @@
 class StaticPagesController < ApplicationController
+  def index
+    @client = Pexels::Client.new(ENV["Pexels"])
+    @images = []
+    if params[:collection_id]
+      begin
+        @images = @client.collections[params[:collection_id]].media
+      rescue
+        @images = []
+      end
+    end
+  end
 end
